65b89fb047d0b8fabd770695d4ea9d7e4dbb6d0a
mbeccuti <beccuti@di.unito.it>
2020-01-19 00:11:56 +0100

    first commit

---
+++ R/bcl2fastq.R
@@ -0,0 +1,72 @@
+#' @title Generating running bcl2fastq
+#' @description This function executes the Illumina bcl2fastq program
+#' @param samplesheet, a character string indicating the Illumina folder where the Samplesheet.csv is located.
+#' @param output.folder, a character string indicating the fastq output folder.
+#' @param threadBCL, a number indicating the number of threads to be used for loading BCL data
+#' @param threadWritingFastq, a number indicating the number of threads to be used for writing FASTQ data
+#'  @param threadDem, a number indicating the number of threads to be used for for processing demultiplexed data
+#'
+#' @return Fastq files
+#' @examples
+#'\dontrun{
+#'     #running rsemstar index for human
+#'     demultiplexing("~/home/data/input","~/home/data/output")
+#'
+#' }
+#' @export
+bcl2fastq <- function(samplesheet,output.folder, threadBCL=1, threadWritingFastq=1, threadDem=6){
+
+  home <- getwd()
+  input.folder=dirname(samplesheet)
+
+  if (!file.exists(samplesheet)){
+    cat(paste("\nIt seems that the ",samplesheet, " file does not exist.\n"))
+    return(1)
+  }
+  setwd(input.folder)
+
+  #running time 1
+  ptm <- proc.time()
+  #running time 1
+
+  #Test docker
+  test <- dockerTest()
+  if(!test){
+      cat("\nERROR: Docker seems not to be installed in your system\n")
+      system("echo 10 > ExitStatusFile 2>&1")
+      setwd(home)
+      return(10)
+  }
+  #Test docker
+
+  #reading docker image names
+  containers.file=paste(path.package(package="RNAseq2"),"/Containers/containersNames.txt",sep="/")
+  containers.names=read.table(containers.file,header=T,stringsAsFactors = F)
+  #reading docker image names
+
+  #Running docker
+  params <- paste("--cidfile ", input.folder,"/dockerID -v ", input.folder,":/input -v ",output.folder,":/output -d ", containers.names["bcl2fastq",1],"  bcl2fastq /input /output /input/",basename(samplesheet)," ",threadBCL," ",threadWritingFastq," ",threadDem, sep="")
+
+  cat("\n\n************ Demultiplexing running ************\n\n")
+  resultRun=dockerRun(params=params)
+
+  if(resultRun==0){
+    cat("\nDemultiplexing is finished\n")
+  }
+  #Running docker
+
+
+  ptm <- proc.time() - ptm
+
+  tmp.run <- NULL
+  tmp.run[length(tmp.run)+1] <- paste("demultiplexing user run time mins ",ptm[1]/60, sep="")
+  tmp.run[length(tmp.run)+1] <- paste("demultiplexing system run time mins ",ptm[2]/60, sep="")
+  tmp.run[length(tmp.run)+1] <- paste("demultiplexing elapsed run time mins ",ptm[3]/60, sep="")
+  writeLines(tmp.run, paste(main.folder,"time.info", sep="/"))
+
+
+  #Storing ExitSatus
+  system("echo 0 > ExitStatusFile 2>&1")
+
+  setwd(home)
+  }
---
+++ R/dockerRun.R
@@ -0,0 +1,69 @@
+#' @title Run docker container
+#' @description This is an internal function executing a docker container. Not to be used by users.
+#' @param params, a character string containing all parameters needed to run the docker container.
+#' @return 0 if success, 1 if parameters are missing, 2 if dockerid file is present, 3 if docker execution fails.
+#' @author Beccuti Marco
+#'
+#' @examples
+#'\dontrun{
+##'     #running runDocker
+#'      dockerRun(params=NULL)
+#'
+#' }
+#' @export
+dockerRun <- function( params=NULL){
+
+    if(is.null(params)){
+        cat("\nNo parameters where provided!\n")
+        system("echo 1 > ExitStatusFile 2>&1")
+        return(1)
+    }
+
+    # to check the Docker ID by file
+    if (file.exists("dockerID")){
+        cat("\n\nDocker does not start, there is already a docker container running che dockerID file!!!\n\n")
+        system("echo 2 > ExitStatusFile 2>&1")
+        return(2)
+    }
+
+    ## to execute docker
+    userid=system("id -u", intern = TRUE)
+    groupid=system("id -g", intern = TRUE)
+    cat(paste("docker run --privileged=true  --user=",userid,":",groupid," ",params,"\n\n", sep=""))
+    system(paste("docker run --privileged=true  --user=",userid,":",groupid," ",params, sep=""))
+
+    ## to obtain the Docker ID by file
+    dockerid=readLines("dockerID", warn = FALSE)
+    cat("\nDocker ID is:\n",substr(dockerid,1, 12),"\n")
+
+    ## to check the Docker container status
+    dockerStatus=system(paste("docker inspect -f {{.State.Running}}",dockerid),intern= T)
+    while(dockerStatus=="true"){
+        Sys.sleep(10);
+        dockerStatus=system(paste("docker inspect -f {{.State.Running}}",dockerid),intern= T)
+        cat(".")
+    }
+    cat(".\n\n")
+    ## to check the Docker container status
+    dockerExit <- system(paste("docker inspect -f {{.State.ExitCode}}",dockerid),intern= T)
+    cat("\nDocker exit status:",dockerExit,"\n\n")
+    if(as.numeric(dockerExit)!=0){
+        system(paste("docker logs ", substr(dockerid,1,12), " &> ", substr(dockerid,1,12),"_error.log", sep=""))
+        cat(paste("\nDocker container ", substr(dockerid,1,12), " had exit different from 0\n", sep=""))
+        cat("\nExecution is interrupted\n")
+        cat(paste("Please send to beccuti@unito.it this error: Docker failed exit 0,\n the description of the function you were using and the following error log file,\n which is saved in your working folder:\n", substr(dockerid,1,12),"_error.log\n", sep=""))
+        system("echo 3 > ExitStatusFile 2>&1")
+        return(3)
+    }
+
+    #saving log and removing docker container
+    file.remove("dockerID")
+    system(paste("docker logs ", dockerid, " >& ", substr(dockerid,1,12),".log", sep=""))
+    system(paste("docker rm -f ",dockerid),intern= T)
+
+    system(paste("cp ",paste(path.package(package="RNAseq2"),"containers/containers.txt",sep="/")," ",getwd(), sep=""))
+
+    #Normal Docker execution
+    system("echo 0 > ExitStatusFile 2>&1")
+    return(0)
+}
